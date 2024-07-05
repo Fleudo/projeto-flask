e6fb4fe8590bff77b46128d24ec79405f7a7727a
Fleudo <93488920+Fleudo@users.noreply.github.com>
2024-07-05 18:38:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, request, jsonify, render_template
 import requests
 import os
 import qrcode
-from io import BytesIO
-from base64 import b64encode
 from dotenv import load_dotenv
-
-load_dotenv()
+from io import BytesIO
+from flask import send_file
 
 app = Flask(__name__)
+
+load_dotenv()
 
 data_url = "https://api.gwmcarclub.com.br/api/associates"
 
@@ -17,12 +17,17 @@ def fetch_associate_by_id(token, associate_id):
         'Authorization': f'Bearer {token}',
         'Content-Type': 'application/json'
     }
+    print(f"Token: {token}")  # Debugging
+    print(f"Fetching associate with ID: {associate_id}")  # Debugging
     response = requests.get(f"{data_url}/{associate_id}", headers=headers)
+    print(f"Response status code: {response.status_code}")  # Debugging
+    print(f"Response text: {response.text}")  # Debugging
     
     if response.status_code == 200:
         try:
             data = response.json()
             if 'data' in data:
+                print(f"Data found: {data['data']}")  # Debugging
                 return data['data']
             else:
                 return None
@@ -45,14 +50,14 @@ def carteirinha(associate_id):
     token = os.getenv("API_TOKEN")
     associate = fetch_associate_by_id(token, associate_id)
     if associate:
-        qr_data = f"Nome: {associate['attributes']['name']}\nCPF: {associate['attributes']['cpf']}\nModelo do Carro: {associate['attributes']['car_model']}"
+        qr_data = f"Nome: {associate['attributes'].get('name', 'N/A')}\nCPF: {associate['attributes'].get('cpf', 'N/A')}\nModelo do Carro: {associate['attributes'].get('car_model', 'N/A')}\nVersão: {associate['attributes'].get('car_version', 'N/A')}\nAno Modelo: {associate['attributes'].get('car_year_model', 'N/A')}\nEmail: {associate['attributes'].get('email', 'N/A')}\nTelefone: {associate['attributes'].get('phone', 'N/A')}"
         qr = qrcode.make(qr_data)
-        buffer = BytesIO()
-        qr.save(buffer, format="PNG")
-        qr_img = b64encode(buffer.getvalue()).decode('utf-8')
-        return render_template('carteirinha.html', associate=associate['attributes'], qr_img=qr_img)
+        qr_io = BytesIO()
+        qr.save(qr_io, 'PNG')
+        qr_io.seek(0)
+        return send_file(qr_io, mimetype='image/png')
     else:
-        return render_template('carteirinha.html', error='Pessoa inexistente na base de dados do clube')
+        return "Pessoa inexistente na base de dados do clube", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
