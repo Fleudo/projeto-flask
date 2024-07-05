b49ac5809a0eb78ca71f008e00d83d1c6cd5a0b7
leudo <engleudo@gmail.com>
2024-07-05 13:59:47 -0300

    Add initial project files

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify, render_template
+import requests
+import os
+
+app = Flask(__name__)
+
+data_url = "https://api.gwmcarclub.com.br/api/associates"
+
+def fetch_associate_by_id(token, associate_id):
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(f"{data_url}/{associate_id}", headers=headers)
+    
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            if 'data' in data:
+                return data['data']
+            else:
+                return None
+        except ValueError:
+            return None
+    else:
+        return None
+
+@app.route('/verify/<int:associate_id>', methods=['GET'])
+def verify(associate_id):
+    token = os.getenv("API_TOKEN")
+    associate = fetch_associate_by_id(token, associate_id)
+    if associate:
+        return render_template('index.html', associate=associate['attributes'])
+    else:
+        return render_template('index.html', error='Pessoa inexistente na base de dados do clube')
+
+if __name__ == '__main__':
+    app.run(debug=True)
