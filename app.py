09f612e8813553982bfbc87c0b2aa0269b86b575
Fleudo <93488920+Fleudo@users.noreply.github.com>
2024-07-05 16:57:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,17 @@ def fetch_associate_by_id(token, associate_id):
         'Authorization': f'Bearer {token}',
         'Content-Type': 'application/json'
     }
+    print(f"Token: {token}")  # Debugging
+    print(f"Fetching associate with ID: {associate_id}")  # Debugging
     response = requests.get(f"{data_url}/{associate_id}", headers=headers)
-    
+    print(f"Response status code: {response.status_code}")  # Debugging
+    print(f"Response text: {response.text}")  # Debugging
+
     if response.status_code == 200:
         try:
             data = response.json()
             if 'data' in data:
+                print(f"Data found: {data['data']}")  # Debugging
                 return data['data']
             else:
                 return None
@@ -28,6 +33,7 @@ def fetch_associate_by_id(token, associate_id):
 @app.route('/verify/<int:associate_id>', methods=['GET'])
 def verify(associate_id):
     token = os.getenv("API_TOKEN")
+    print(f"Token: {token}")  # Debugging
     associate = fetch_associate_by_id(token, associate_id)
     if associate:
         return render_template('index.html', associate=associate['attributes'])
